3405491dcce8cfe319f885f0a11b903d0fe10dda
shurikhot <shurikhot2@gmail.com>
2024-05-16 11:15:18 +0300

    fix. commit before meilisearch

--- routes/web.php
+++ routes/web.php
@@ -2,13 +2,16 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', 'App\Http\Controllers\PetController@index')->name('home');
-Route::get('/pets/{id}', 'App\Http\Controllers\PetController@single')->name('single')->where(['id' => '[0-9]+']);
-Route::get('/pets/{species}', 'App\Http\Controllers\PetController@show')->name('pets');
+Route::group(['namespace' => 'App\Http\Controllers'], function () {
+    Route::get('/', 'PetController@index')->name('home');
+    Route::get('/pets/{id}', 'PetController@single')->name('single')->where(['id' => '[0-9]+']);
+    Route::get('/pets/{species}', 'PetController@show')->name('pets');
 
-Route::post('/pets/{id}/favorite', 'App\Http\Controllers\UserController@addFavorite')->name('addFavorite')->where(['id' => '[0-9]+']);
-Route::post('/pets/{id}/remove', 'App\Http\Controllers\UserController@remFavorite')->name('remFavorite')->where(['id' => '[0-9]+']);
-Route::any('/account/{item}', 'App\Http\Controllers\UserController@account')->name('account');
+    Route::post('/pets/{id}/favorite', 'UserController@addFavorite')->name('addFavorite')->where(['id' => '[0-9]+']);
+    Route::post('/pets/{id}/remove', 'UserController@remFavorite')->name('remFavorite')->where(['id' => '[0-9]+']);
+    Route::any('/account/{item}', 'UserController@account')->name('account');
+});
+
 Route::view('/guardianship', 'front.pages.guardianship', ['cust_title' => ' :: Опікунство'])->name('guardianship');
 Route::view('/volunteer', 'front.pages.volunteer', ['cust_title' => ' :: Волонтерство'])->name('volunteer');
 Route::view('/about', 'front.pages.about', ['cust_title' => ' :: Про нас'])->name('about');
@@ -25,13 +28,12 @@ Route::group(['prefix' => 'admin', 'namespace' => 'App\Http\Controllers\Admin', 
     Route::resource('/sliders', 'SliderController');
 });
 
-Route::group(['middleware' => 'guest'], function () {
-    Route::get('register', 'App\Http\Controllers\UserController@create')->name('register.create');
-    Route::post('register', 'App\Http\Controllers\UserController@store')->name('register.store');
-    Route::get('login', 'App\Http\Controllers\UserController@loginForm')->name('login.create');
-    Route::post('login', 'App\Http\Controllers\UserController@login')->name('login');
+Route::group(['middleware' => 'guest', 'namespace' => 'App\Http\Controllers'], function () {
+    Route::get('register', 'UserController@create')->name('register.create');
+    Route::post('register', 'UserController@store')->name('register.store');
+    Route::get('login', 'UserController@loginForm')->name('login.create');
+    Route::post('login', 'UserController@login')->name('login');
 });
-
 Route::get('logout', 'App\Http\Controllers\UserController@logout')->name('logout')->middleware('auth');
 
 /* API-practice */
